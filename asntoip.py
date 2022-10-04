ca2216833acfaf7d4be56d8555d8042d76ad5e31
YTTA <1qtofc@gmail.com>
2022-10-04 22:42:15 +0700

    save new api

--- asntoip.py
+++ asntoip.py
@@ -27,6 +27,9 @@ def removeduplicate(path):
 
 def asnToIP(asn):
 
+    # another api
+    # r = requests.get(f'https://asn.ipinfo.app/list/{asn}').json()
+
     r = requests.get(f'https://api.bgpview.io/asn/{asn}/prefixes').json()
     data = r['data']['ipv4_prefixes']
     
